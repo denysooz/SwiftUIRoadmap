c53241e7a53d93ccff24f55c3f6246ada8495f31
denysooz <den.darevskiy@mail.ru>
2024-09-22 12:59:10 +0300

    Initial Commit

---
+++ SwiftUIRoadmap/SwiftUIRoadmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRoadmapApp.swift
+//  SwiftUIRoadmap
+//
+//  Created by Denis Dareuskiy on 22.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRoadmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
